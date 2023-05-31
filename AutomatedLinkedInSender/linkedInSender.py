0379db1e9a1ed7434f489622d56e82e26aa9d6e5
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-31 17:28:42 +0800

    Added environmental variables, webdriver, and explicit wait

--- AutomatedLinkedInSender/linkedInSender.py
+++ AutomatedLinkedInSender/linkedInSender.py
@@ -1,16 +1,29 @@
+# Importing necessary libraries for environment variables
+from dotenv import load_dotenv
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
+# Credentials via env
+load_dotenv()
+account_name = os.getenv["ACCOUNT_NAME"]
+account_password = os.getenv["ACCOUNT_PASSWORD"]
 
+# Explicit Wait
+def document_initialised(driver):
+    return driver.execute_script("return initialised")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("C:\Program Files\Google\Chrome\Application\chrome.exe")
 
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3618090331&geoId=103121230&keywords=it%20intern&location=Philippines&refresh=true")
 
 title = driver.title
 assert title == "Web form"
 
+WebDriverWait(driver, timeout=10).until(document_initialised)
+el = driver.find_element(By.TAG_NAME, "p")
 driver.implicitly_wait(0.5)
 
 text_box = driver
