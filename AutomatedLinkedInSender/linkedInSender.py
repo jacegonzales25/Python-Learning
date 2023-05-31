41cb98a9214a7878e4354ad45a201c92fc61ebae
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-31 13:18:09 +0800

    Initialized Selenium web driver and link

---
+++ AutomatedLinkedInSender/linkedInSender.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3618090331&geoId=103121230&keywords=it%20intern&location=Philippines&refresh=true")
