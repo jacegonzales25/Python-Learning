3832a51fa817a69f2b7cc9239a09138e6d3b3367
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-31 20:01:19 +0800

    Added finding email and password field and submit, webDriverWait for rendering main page of LinkedIn jobs

--- AutomatedLinkedInSender/linkedInSender.py
+++ AutomatedLinkedInSender/linkedInSender.py
@@ -4,6 +4,7 @@ import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
 # Credentials via env
@@ -16,14 +17,27 @@ def document_initialised(driver):
     return driver.execute_script("return initialised")
 
 driver = webdriver.Chrome("C:\Program Files\Google\Chrome\Application\chrome.exe")
-
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3618090331&geoId=103121230&keywords=it%20intern&location=Philippines&refresh=true")
 
 title = driver.title
 assert title == "Web form"
 
+sign_in_button = driver.find_element_by_link_text("Sign in")
+sign_in_button.click()
+
 WebDriverWait(driver, timeout=10).until(document_initialised)
 el = driver.find_element(By.TAG_NAME, "p")
-driver.implicitly_wait(0.5)
+# driver.implicitly_wait(0.5)
 
-text_box = driver
+email_field = driver.find_element_by_id("username")
+email_field.send_keys(account_name)
+password_field = driver.find_element_by_id("password")
+password_field.send_keys(account_password)
+password_field.send_keys(Keys.ENTER)
+
+WebDriverWait(driver, timeout=10).until(document_initialised)
+el = driver.find_element(By.TAG_NAME, "p")
+
+# Application Button
+apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+apply_button.click()
